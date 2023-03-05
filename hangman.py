9432992d2229e8ef284f5ac5f348edda4a9653bb
thong91 <nmthong220591@gmail.com>
2023-03-05 15:38:54 +0700

    Update letter for hangman

--- hangman.py
+++ hangman.py
@@ -4,27 +4,27 @@ import random
 play = True
 choose_category=0
 rand_word=""
-guess_times = 5 #5 time to guess
 play_number = 0
 win_number = 0
-topic = [["car", "caro", "carole"],#topic 1
-         ["dog", "doga", "dogalo"],#topic 2
-         ["apple", "appleni", "applenol"],#topic 3
-         ["hot", "cold", "coldest"]]#topic 4
+letter = []  # Slit ramdom word to separately letter
+topic = [["Russia","Germany","United Kingdom","France","Italy","Spain","Ukraine","Poland","Netherlands","Finland","Vietnam","Thailan","Lao","Campodia","China"],#topic 1
+         ["Audi","BMW","Bentley","Chevrolet","Dodge","Ford","Honda","Hyundai","Infiniti","Jaguar","Jeep","Kia","Land Rover","Lexus","Lincoln"],#topic 2
+         ["Dog","Bear","Elephant","Polar bear","Turtle","Tortoise","Crocodile","Rabbit","Porcupine","Har","Hen","Pigeon","Albatross","Crow","Fish"],#topic 3
+         ["apples","pears","oranges","grapefruits","mandarins","limes","apricots","peaches","plums","tropical","exotic","bananas","mangoes","berries","strawberries"]]#topic 4
 
 #Define for each topic    
 def def_category(choose_category):
     if choose_category == 1:
-        return ("Cars")
+        return ("Countries")
     if choose_category == 2:
-        return ("Dogs")
+        return ("Car brands")
     if choose_category == 3:
-        return ("Apples")
+        return ("Animals")
     if choose_category == 4:
-        return ("Weather")
+        return ("Fruits")
 
 #Draw function
-def visuals(player_lives):
+def draw_hangman(player_lives):
 	if player_lives == 1:
 		return"""
 		_______
@@ -100,28 +100,47 @@ def visuals(player_lives):
 		"""	
 
 #Main function
-def game_start(rand_word):
+def game_start(letter):
     count = 0
     winner = False
+    ori_len = len(letter) # Original leng of letter
+    results=[]
     while count < 5:
-        guess_word = input("Player 2 guess: ")
-        if guess_word==rand_word:
-            winner = True
-            break
-        else:
-            count += 1
-            print(visuals(count))
+        guess_letter = input("Player 2 guess alphabet letter of random word: ") #alphabet letter (a,b,c...)
+        #print("DEbug",letter)
+        for c in range(len(letter)): #go through position of array
+            if guess_letter.lower()==letter[c]:
+                results.append(letter[c])
+                print("That's righ letter. Keep going:")
+                print("Word: ",results)
+                letter.pop(c)
+                #print("DEbug",len(results),ori_len)
+                if len(results)==ori_len:
+                    winner=True
+                    #print("DEbug",len(results),ori_len)
+                    return winner
+                break
+            else:
+                count += 1
+                print("That's wrong letter. Try again:")
+                print(draw_hangman(count))
+                print("Word: ",results)
+                break    
     return winner
 
-#Main program
 while play == True:
     start = int(input("1. Play\n2. Exit")) #only number 
+    letter.clear() #reset letter 
+    #print("DEbug",letter)
     if start == 1:
-        choose_category = int(input("\nPlayer 1 enter topic number (1.Cars; 2.Dogs; 3.Apples;4.Weather) : "))
+        choose_category = int(input("\nPlayer 1 enter topic number 1.countries; 2.car brands; 3.animals; 4.fruits: "))
         print("Your category is: ",def_category(choose_category))
         rand_word = random.choice(topic[choose_category - 1]) #Random choose one of the word in this topic , -1 because first position in array is 0
-        print ("Your random word is: ", rand_word)
-        if game_start(rand_word) == False:
+        print ("Hint!: Your random word is ", rand_word)
+        print ("--------------------------------------------")
+        for i in rand_word.lower():
+            letter.append(i)
+        if game_start(letter) == False:
             print("Player 2 LOSE! Do you want to play again?")
         else:
             print("Player 2 WIN!  Do you want to play again?")
@@ -131,3 +150,4 @@ while play == True:
         play = False
         print("How many time did you play: ",play_number)
         print("How many time did you win: ",win_number)
+        
