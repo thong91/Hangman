f79dfe618ff565ab5062d1a874172eaf6841d271
Nguyen Minh Thong <126743684+thong91@users.noreply.github.com>
2023-03-10 17:58:51 +0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -139,37 +139,37 @@ print ("------GAME HANGEMAN-------")
 while play == True:
     letter.clear() #reset letter 
     #---input topic
-    #try:
-    print ("--------------------------------------------")
-    start = int(input("1. Play\n2. Exit\nChoose: ")) #only number 
-    if start == 1:
-        choose_category = int(input("\nPlayer 1 enter topic number 1.countries; 2.car brands; 3.animals; 4.fruits: \nChoose: "))
-        if def_category(choose_category)==None:
+    try:
+        print ("--------------------------------------------")
+        start = int(input("1. Play\n2. Exit\nChoose: ")) #only number 
+        if start == 1:
+            choose_category = int(input("\nPlayer 1 enter topic number 1.countries; 2.car brands; 3.animals; 4.fruits: \nChoose: "))
+            if def_category(choose_category)==None:
+                print("Your input is invalid! Play again!")
+                continue
+            print("Your category is: ",def_category(choose_category))
+            rand_word = random.choice(topic[choose_category - 1]) #Random choose one of the word in this topic , -1 because first position in array is 0
+            print ("Hint!: The 3 random word's letter:", rand_word[0],rand_word[1],rand_word[2])
+            print ("--------------------------------------------")
+            for i in rand_word.lower():
+                letter.append(i)
+            if game_start(letter) == False:
+                print("The random word is: ",rand_word)
+                print("Player 2 LOSE! Do you want to play again?")
+                print ("--------------------------------------------")
+            else:
+                print("The random word is: ",rand_word)
+                print("Player 2 WIN!  Do you want to play again?")
+                print ("--------------------------------------------")
+                win_number += 1
+            play_number += 1
+        elif start == 2:
+            play = False
+            print("---------Results--------")
+            print("How many time did you play: ",play_number)
+            print("How many time did you win: ",win_number)
+        else:
             print("Your input is invalid! Play again!")
             continue
-        print("Your category is: ",def_category(choose_category))
-        rand_word = random.choice(topic[choose_category - 1]) #Random choose one of the word in this topic , -1 because first position in array is 0
-        print ("Hint!: The 3 random word's letter:", rand_word[0],rand_word[1],rand_word[2])
-        print ("--------------------------------------------")
-        for i in rand_word.lower():
-            letter.append(i)
-        if game_start(letter) == False:
-            print("The random word is: ",rand_word)
-            print("Player 2 LOSE! Do you want to play again?")
-            print ("--------------------------------------------")
-        else:
-            print("The random word is: ",rand_word)
-            print("Player 2 WIN!  Do you want to play again?")
-            print ("--------------------------------------------")
-            win_number += 1
-        play_number += 1
-    elif start == 2:
-        play = False
-        print("---------Results--------")
-        print("How many time did you play: ",play_number)
-        print("How many time did you win: ",win_number)
-    else:
-        print("Your input is invalid! Play again!")
-        continue
-    #except:
-    #    print("Your input is invalid! Play again!")   
+    except:
+        print("Your input is invalid! Play again!")   
